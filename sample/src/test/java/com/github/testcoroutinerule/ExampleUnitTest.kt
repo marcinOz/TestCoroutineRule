d0874953ba7ed63cac3237d1e4d03a6bcaf6cc93
Marcin Oziemski <marcin.oziemski@netguru.pl>
2019-07-07 12:44:56 +0200

    example Unit test and fix for exporting TestCoroutineRule.kt

--- sample/src/test/java/com/github/testcoroutinerule/ExampleUnitTest.kt
+++ sample/src/test/java/com/github/testcoroutinerule/ExampleUnitTest.kt
@@ -1,6 +1,10 @@
 package com.github.testcoroutinerule
 
+import com.github.testcoroutinesrule.TestCoroutineRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
 /**
@@ -9,8 +13,17 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addition_isCorrect() = testCoroutineRule.runBlockingTest {
+        val result = testFun()
+        assertEquals(result, 4)
+    }
+
+    suspend fun testFun() = withContext(Dispatchers.Unconfined) {
+        2 + 2
     }
 }
